7771696187056e06ccbf358ffa7bc5ce53dc7450
Ahmed Gamal <ahmedgamal124720@gmail.com>
2025-07-18 04:38:15 +0300

    preset

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,9 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
-import { MyPreset } from './mypresets';
 import { provideHttpClient } from '@angular/common/http';
+import { definePreset } from '@primeng/themes';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,11 +15,46 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: MyPreset,
-        options: {
-          darkModeSelector: '.my-add-dark'
-        },
+        preset: definePreset(Aura, {
+          semantic: {
+            colorScheme: {
+              light: {
+                surface: {
+                  50: '{zinc.50}',
+                  100: '{zinc.100}',
+                  200: '{zinc.200}',
+                  300: '{zinc.300}',
+
+                  color: '{slate.100}',
+                  inverseColor: '#ffffff',
+                  hoverColor: '{slate.100}',
+                  activeColor: '{slate.950}',
+                },
+                primary: {
+                  50: '{slate.50}',
+                  100: '{slate.100}',
+                  200: '{slate.200}',
+                  300: '{slate.300}',
+                  400: '{zinc.400}',
+                  500: '{zinc.500}',
+                  600: '{zinc.600}',
+                  700: '{zinc.700}',
+                  800: '{zinc.800}',
+                  900: '{zinc.900}',
+                  950: '{zinc.950}',
+                  color: '{slate.700}',
+                  inverseColor: '{slate.900}',
+                  hoverColor: '{slate.900}',
+                  activeColor: '{slate.100}',
+                },
+                options: {
+                  darkModeSelector: '.my-add-dark'
+                },
+              }
+            },
+          }
+        })
       }
-    }),
+    })
   ]
 };
